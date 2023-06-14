f708f2c05e19a2cc6f95d79a46c0b67698532f3e
abel689 <joaquin.rivas.canales@alumnos.sip.cl>
2023-06-14 13:44:52 -0400

    cambio8

--- inicio.js
+++ inicio.js
@@ -12,3 +12,43 @@ cartCount.textContent = itemCount;
 
 // Asignar evento de clic al Ã­cono del carrito
 cartIcon.addEventListener('click', addToCart);
+
+
+/* carrusel */
+
+const carousel = document.querySelector('.carousel');
+const carouselItems = document.querySelectorAll('.carousel-item');
+const totalItems = carouselItems.length;
+let currentIndex = 0;
+
+function showItem(index) {
+if (index < 0 || index >= totalItems) {
+    return;
+}
+
+carouselItems.forEach(item => {
+    item.style.transform = `translateX(-${index * 100}%)`;
+});
+}
+
+function nextItem() {
+currentIndex++;
+if (currentIndex >= totalItems) {
+    currentIndex = 0;
+}
+showItem(currentIndex);
+}
+
+function previousItem() {
+currentIndex--;
+if (currentIndex < 0) {
+    currentIndex = totalItems - 1;
+}
+showItem(currentIndex);
+}
+
+setInterval(nextItem, 3000); // Cambiar automáticamente cada 3 segundos
+
+// Event Listeners para botones de navegación
+document.getElementById('btn-prev').addEventListener('click', previousItem);
+document.getElementById('btn-next').addEventListener('click', nextItem);
